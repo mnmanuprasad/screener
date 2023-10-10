bab1a72f2ef6796f4aa33277fd188630254115f6
mnmanuprasad <mnmanuprasad01@gmail.com>
2023-10-10 21:22:55 +0530

    v1

--- contentScript.js
+++ contentScript.js
@@ -10,16 +10,16 @@
             startRecording(response);
         }
         if(obj['type']=='STOP'){
-            stopRecording(response);
+            stopRecording();
         }
         
     })
 
-    async function stopRecording(response){
+    async function stopRecording(){
         mediaRecorder.stop();
     }
 
-    async function startRecording(response){
+    async function startRecording(){
         stream = await navigator.mediaDevices.getDisplayMedia({'video': true});
         mediaRecorder = new MediaRecorder(stream);
 
@@ -33,7 +33,13 @@
             console.log("Recording has been stoppped");
             videoBlob = new Blob(recordedChunks, {type: 'video/webm'})
             const recordURL = URL.createObjectURL(videoBlob);
-           
+            console.log(recordURL)
+
+            var elem = window.document.createElement('a');
+            elem.href = window.URL.createObjectURL(videoBlob);
+            elem.download = 'RecordedVideo2.webm';        
+            document.body.appendChild(elem);
+            elem.click();       
         }
         mediaRecorder.start();
     }
--- popup.js
+++ popup.js
@@ -2,29 +2,32 @@ import { getActiveTab } from "./utils.js";
 
 (function(){
 
-        // const startRecordBtn = document.getElementById('start-record');
-        // const stopRecordBtn = document.getElementById('stop-record');
+        const startRecordBtn = document.getElementById('start-record');
+        const stopRecordBtn = document.getElementById('stop-record');
 
-        // startRecordBtn.addEventListener('click',async ()=>{
-        //     let tab = await getActiveTab();
-        //     chrome.tabs.sendMessage(tab.id, {type: 'START'}, downloadVideo) 
-        // })
+        startRecordBtn.addEventListener('click',async ()=>{
+            console.log('Started : popup')
+            let tab = await getActiveTab();
+            chrome.tabs.sendMessage(tab.id, {type: 'START'}) 
+        })
         
-        // startRecordBtn.addEventListener('click', async()=>{
-        //     let tab = await getActiveTab();
-        //     chrome.tabs.sendMessage(tab.id, {type: 'STOP'}, downloadVideo)
-        // })
+        stopRecordBtn.addEventListener('click', async()=>{
+            console.log('stopped: popoup')
+            let tab = await getActiveTab();
+            chrome.tabs.sendMessage(tab.id, {type: 'STOP'})
+        })
 })();
 
-function downloadVideo(url){
-    if (url && typeof url === 'string') {
-        chrome.downloads.download({
-          saveAs: true,
-          url: url
-        });
-    }
-    else(
-        console.log(url)
-    );
+// function downloadVideo(url){
+//     if (url && typeof url === 'string') {
+//         chrome.downloads.download({
+//           saveAs: true,
+//           url: url
+//         });
+        
+//     }
+//     else(
+//         console.log(url)
+//     );
   
-}
+// }
